20bb5e171ac2195564756973e81f8b78fd1d4dad
alexlecco <alexlecco24@gmail.com>
2017-03-09 21:05:50 -0300

    [Layout component was created in index.js]

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,24 @@
-require('./modulo1.js');
-require('./modulo2.js');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  constructor() {
+    super();
+    this.nombre = "Alex Villecco";
+  }
+
+  obtenerNombre() {
+    return "Alex Lecco";
+  }
+
+  render() {
+    return(
+      // <h1>Hola {this.nombre}</h1>
+      <h1>Hola {this.obtenerNombre()}</h1>
+    )
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout />, app);
